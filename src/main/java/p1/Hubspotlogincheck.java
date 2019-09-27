2002928af06ae327b5bf2a725c96bc213d06f588
vishnu <vishnutej586@gmail.com>
2019-09-27 12:03:28 +0530

    updated pom

--- src/main/java/p1/Hubspotlogincheck.java
+++ src/main/java/p1/Hubspotlogincheck.java
@@ -4,14 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 //import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hubspotlogincheck {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "F:\\Vishnu Tej\\chromedriver.exe");
-		//WebDriverManager.chromedriver().setup();
+		//System.setProperty("webdriver.chrome.driver", "F:\\Vishnu Tej\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://app.hubspot.com/login");
 		try {
